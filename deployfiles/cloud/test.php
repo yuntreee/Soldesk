d1a2bc40fb0377dae8e39e8b9ed2b995b0e25f46
yuntreee <lewis45626@gmail.com>
2021-12-09 13:08:39 +0900

    TEST

--- deployfiles/cloud/test.php
+++ deployfiles/cloud/test.php
@@ -6,8 +6,8 @@
     $mysqli = new mysqli($host, $user, $pw, $dbName);
  
     if($mysqli){
-        echo "MySQL 접속 성공";
+        echo "MySQL 접속 성공!";
     }else{
-        echo "MySQL 접속 실패";
+        echo "MySQL 접속 실패!";
     }
 ?>
