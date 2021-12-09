eba1031b659d53d8778c744c744f607133bf6df5
yuntreee <lewis45626@gmail.com>
2021-12-09 12:02:15 +0900

    TEST

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,6 +4,9 @@ apt install -y apache2
 apt install libapache2-mpm-itk
 apt install -y php7.2
 apt install -y php-mbstring php-gd php-curl php-xml php-mysql
+cd /var/www/html
+curl -sS https://getcomposer.org/installer | php
+php composer.phar require aws/aws-sdk-php
 #Set Environment
 sed -i 's/;date.timezone =/date.timezone = Asia\/Seoul/g' /etc/php/7.2/apache2/php.ini
 sed -i 's/;date.timezone =/date.timezone = Asia\/Seoul/g' /etc/php/7.2/cli/php.ini
