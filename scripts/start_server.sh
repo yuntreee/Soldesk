b6e732447af582f3352064f6245e46393432cf33
YoungHoYun <60495897+yuntreee@users.noreply.github.com>
2021-12-30 12:32:39 +0900

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,7 +3,6 @@ cd /var/www/html
 curl -sS https://getcomposer.org/installer | php
 php composer.phar require aws/aws-sdk-php
 
-chown ubuntu:ubuntu /var/www/html
 
 sed -i 's/;date.timezone =/date.timezone = Asia\/Seoul/g' /etc/php/7.2/apache2/php.ini
 sed -i 's/;date.timezone =/date.timezone = Asia\/Seoul/g' /etc/php/7.2/cli/php.ini
